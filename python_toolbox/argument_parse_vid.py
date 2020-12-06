9f68332a81d7a58b9fd5599dae210dcc9e489070
Mekrab <65715966+Mekrab@users.noreply.github.com>
2020-12-06 10:05:42 -0700

    argument parse
    construct the argument parser and parse the arguments. YOLO object detector trained on COCO dataset (XXXXXX classes)

---
+++ python_toolbox/argument_parse_vid.py
@@ -0,0 +1,47 @@
+# import the necessary packages
+from pyimagesearch import social_distancing_config as config
+from pyimagesearch.detection import detect_people
+from scipy.spatial import distance as dist
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default="",
+	help="path to (optional) input video file")
+ap.add_argument("-o", "--output", type=str, default="",
+	help="path to (optional) output video file")
+ap.add_argument("-d", "--display", type=int, default=1,
+	help="whether or not output frame should be displayed")
+args = vars(ap.parse_args())
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([config.MODEL_PATH, "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([config.MODEL_PATH, "yolov3.weights"])
+configPath = os.path.sep.join([config.MODEL_PATH, "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (XXXXXX classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# check if we are going to use GPU
+if config.USE_GPU:
+	# set CUDA as the preferable backend and target
+	print("[INFO] setting preferable backend and target to CUDA...")
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream and pointer to output video file
+print("[INFO] accessing video stream...")
+vs = cv2.VideoCapture(args["input"] if args["input"] else 0)
+writer = None
