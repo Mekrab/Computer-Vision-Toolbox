d65953f6ad17240ea7e1776d3f172ac88567ae76
Mekrab <65715966+Mekrab@users.noreply.github.com>
2020-12-06 11:33:14 -0700

    Push physical devices list for GPU

---
+++ python_toolbox/gpu_workaround.py
@@ -0,0 +1,11 @@
+#Push physical devices list for GPU
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+# Disable all GPUS
+    tf.config.set_visible_devices([], 'GPU')
+    visible_devices = tf.config.get_visible_devices()
+    for device in visible_devices:
+        assert device.device_type != 'GPU'
+except:
+# Invalid device or cannot modify virtual devices once initialized.
+    pass
